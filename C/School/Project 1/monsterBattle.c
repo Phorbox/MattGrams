4de649e58c41f27bf3e782bef64e4b21287f061b
Phorbox <daboxmasta@gmail.com>
2020-02-24 00:01:34 -0600

    Finished Project 1 - Monster Battler
    Monster battler is a simulation of a delve into a dungeon with many doors filled with fabulous monsters and terrifying prizes!

--- C/School/Project 1/monsterBattle.c
+++ C/School/Project 1/monsterBattle.c
@@ -1,44 +1,153 @@
+#include <stdio.h>
 
+//Enumerates the types of rooms
 typedef enum TYPE { EMPTY, PRIZE, MONSTER} TYPE; 
 
-typedef struct Character
+//Struct to represent monsters and players.
+typedef struct Entity_struct
 {
 	int hitPoints;
 	int experiencePoints;
-}
+} Entity;
 
-typedef struct Prize
+//struct to represent prize/treasure rooms
+typedef struct Prize_struct
 {
 	int points;
+
 } Prize;
 
-typedef struct Room
+//stuct to represent rooms, named encounter for readability
+typedef struct Room_struct
 {
 	TYPE type;
-	Character monster;
+	Entity monster;
 	Prize prize;
-} Room;
+} Encounter;
 
+//generates a random number between min and max inclusive
 int getRandomNumber(int min, int max)
 {
 	int n;	
-		//32 - 8 = 24
-	n = (rand() % (max-min)) + min
+
+	n = (rand() % (max-min)) + min;
 
 	return n;
 }
 
+//fills the current room
+void fillRoom(Encounter* currentRoom )
+{
+		
+	int reward = 0;
+	
+	int typeR = getRandomNumber(0,9);
 
-void fillRoom(Room *rooms , int roomID){
-	// make dynamic ex: if the types of room changes.
-	*rooms[roomID]->TYPE = getRandomNumber(0,2);
-
-
-
-
+	//populate monster
+	if(typeR == 0)
+	{
+		currentRoom->type = 0;
+	}else if(typeR < 5)
+	//populate prize
+	
+	{
+		currentRoom->type = 1;
+		reward = getRandomNumber(5,10);
+		currentRoom->prize.points = reward;
+ 	}else 
+	 
+	{
+		currentRoom->type = 2;
+		int HP = getRandomNumber(10,30);
+		
+		if((HP % 3) == 0)
+		{
+			reward = 1;
+		}
+		currentRoom->monster.hitPoints = HP;
+		currentRoom->monster.experiencePoints = reward;	
+	}
+	 
 }
 
-
-void fillDungeon(){
-
+//applies fill room to the entire dungeon
+void fillDungeon(Encounter* room , int length)
+{
+	int i;
+	for(i = 0 ; i < length ; i++ )
+	{
+		
+		fillRoom(&room[i]);
+		
+	}
 }
+
+//announces the status of the player
+void printPlayer(Entity* player)
+{
+	printf("\nPlayer (HP: %d, XP: %d)\n", player->hitPoints , player->experiencePoints);
+}
+
+//operates the player by the rooms info
+void enterRoom(Entity* player , Encounter* currentRoom )
+{
+	//Enter Monster room
+	if(currentRoom->type == 2)
+	{
+		printf("a monster! (HP: %d , XP: %d)\n", currentRoom->monster.hitPoints , currentRoom->monster.experiencePoints);
+		player->hitPoints -= currentRoom->monster.hitPoints;
+		player->experiencePoints += currentRoom->monster.experiencePoints;
+		printf("You lost %d hitpoints from this room\n" , currentRoom->monster.hitPoints);
+	} else if(currentRoom->type == 1)
+
+	//Enter Prize room
+	
+	{
+		printf("a glorious prize!\n");
+		(*player).hitPoints += currentRoom->prize.points;
+	} else if(currentRoom->type == 0)
+
+	//Enter Empty room
+	
+	{
+		printf("cold nothingness, what will the next room hold?\n");
+
+	}
+	printPlayer(player);
+}
+
+//apples enterRoom and the player to the entire dungeon
+void enterDungeon(Entity* player , Encounter* room , int length)
+{
+	int i = 0;
+	
+	while(i < length)
+	{
+		//Defeat Check
+		if (player->hitPoints <= 0)
+		{
+			printf("You did not survive the monster battle! GAME OVER.\n");
+			printf("You had earned %d experience, and entered %d doors\n", player->experiencePoints , i);
+			i += length;
+		} 
+		//open door
+		else 
+		{
+			
+			printf("you open the door to room #%d, and find...\n\n", i + 1);
+			enterRoom(player , &room[i]);
+			
+		}
+
+		i++;
+	}
+
+	// Victory read the closing statement
+	if(player->hitPoints > 0)
+	{
+		printf("You did survive the monster battle! VICTORIOUS.\n");
+		printf("you had earned %d experience, and entered %d doors\n", player->experiencePoints , i);
+
+	}
+}
+
